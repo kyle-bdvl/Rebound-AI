c8dac58cbc955e02c262019203deea97380fcc23
Kyle-bdvl <kyle.boudville@gmail.com>
2026-03-27 09:09:18 +0800

    Testing out how to commit to github

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { useMemo, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useAppDispatch } from "@/store/hooks"
 import { setInitialPrompt } from "@/store/chat"
-
+// Testing out git/github 
 /**
  * ShadCN UI components
 
